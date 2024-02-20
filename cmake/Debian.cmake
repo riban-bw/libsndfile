66cb4e75117a628d4f3bc7d1a8dce68b22a2d2d1
riban <brian@riban.co.uk>
2024-02-20 18:13:11 +0000

    Changes to CMake to build Debian package

---
+++ cmake/Debian.cmake
@@ -0,0 +1,4 @@
+SET(CPACK_GENERATOR "DEB")
+SET(CPACK_DEBIAN_PACKAGE_NAME "libsndfile1-dev")
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Zynthian")
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>= 2.4), libflac8 (>= 1.3.0), libogg0 (>= 1.0rc3), libvorbis0a (>= 1.2.3), libvorbisenc2 (>= 1.2.3)")
